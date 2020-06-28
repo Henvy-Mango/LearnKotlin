651871085ce85452add8ede6946112cb33e3b088
Naomi <mangogo.naomi@qq.com>
2020-06-29 01:52:52 +0800

    init

---
+++ activity/src/main/java/com/example/kotlin/ThirdActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+
+
+}
