3a27b3dd4855cec92c481c81b9f216cb23cb68dc
Mangogo <807082193@qq.com>
2020-06-30 12:01:19 +0800

    sealed class add lateinit

--- junior/src/main/java/com/example/junior/until/Msg.kt
+++ junior/src/main/java/com/example/junior/until/Msg.kt
@@ -21,22 +21,25 @@ class Msg(val content: String, val type: Int) {
     }
 }
 
-class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+sealed class MsgViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-    inner class LeftViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val leftMsg: TextView = view.findViewById(R.id.msg_left)
-    }
+class LeftViewHolder(view: View) : MsgViewHolder(view) {
+    val leftMsg: TextView = view.findViewById(R.id.msg_left)
+}
 
-    inner class RightViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val rightMsg: TextView = view.findViewById(R.id.msg_right)
-    }
+class RightViewHolder(view: View) : MsgViewHolder(view) {
+    val rightMsg: TextView = view.findViewById(R.id.msg_right)
+}
+
+class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<MsgViewHolder>() {
+
 
     override fun getItemViewType(position: Int): Int {
         val msg = msgList[position]
         return msg.type
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
         if (viewType == Msg.TYPE_RECEIVED) {
             val view =
                 LayoutInflater.from(parent.context).inflate(R.layout.msg_left_item, parent, false)
@@ -50,12 +53,11 @@ class MsgAdapter(val msgList: List<Msg>) : RecyclerView.Adapter<RecyclerView.Vie
 
     override fun getItemCount() = msgList.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MsgViewHolder, position: Int) {
         val msg = msgList[position]
         when (holder) {
             is LeftViewHolder -> holder.leftMsg.text = msg.content
             is RightViewHolder -> holder.rightMsg.text = msg.content
-            else -> throw IllegalArgumentException()
         }
     }
 
