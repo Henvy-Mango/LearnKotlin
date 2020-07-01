f8718c589e54214fb87d4f397e7b9b78f05dd6b0
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2020-07-01 20:58:40 +0800

    fragment finish

--- fragmentactivity/src/main/java/com/example/fragmentactivity/NewsApp/NewsContentActivity.kt
+++ fragmentactivity/src/main/java/com/example/fragmentactivity/NewsApp/NewsContentActivity.kt
@@ -1,12 +1,34 @@
 package com.example.fragmentactivity.NewsApp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fragmentactivity.R
+import kotlinx.android.synthetic.main.activity_news_content.*
 
 class NewsContentActivity : AppCompatActivity() {
+
+    companion object {
+        fun actionStart(context: Context, news: News) {
+            val intent = Intent(context, NewsContentActivity::class.java).apply {
+                putExtra("new_title", news.title)
+                putExtra("new_content", news.content)
+            }
+            context.startActivity(intent)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_content)
+
+        val title = intent.getStringExtra("new_title")
+        val content = intent.getStringExtra("new_content")
+        if (title != null && content != null) {
+            val fragment = newsContentFrag as NewsContentFragment
+            fragment.refresh(News(title, content))
+        }
     }
 }
