b7597ddf69fbf298ddcfe4444b9c5e79c561e942
Naomi <mangogo.naomi@qq.com>
2020-07-01 20:58:40 +0800

    fragment finish

--- fragmentactivity/src/main/java/com/example/fragmentactivity/NewsApp/News.kt
+++ fragmentactivity/src/main/java/com/example/fragmentactivity/NewsApp/News.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fragmentactivity.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.news_content_frag.*
+import kotlinx.android.synthetic.main.news_title_frag.*
 
 /**
  * FileName: News
@@ -33,3 +38,76 @@ class NewsContentFragment() : Fragment() {
     }
 
 }
+
+class NewsTitleFragment() : Fragment() {
+
+    private var isTwoPane: Boolean = false
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.news_title_frag, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        isTwoPane = activity?.findViewById<View>(R.id.newsContentLayout) != null
+        val layoutManager = LinearLayoutManager(activity)
+        newsTitleRecyclerView.layoutManager = layoutManager
+        val adapter = NewsAdapter(getNews())
+        newsTitleRecyclerView.adapter = adapter
+    }
+
+    private fun getNews(): List<News> {
+        val newsList = ArrayList<News>()
+        for (i in 1..50) {
+            val news = News("This is news title $i", getRadomLengthString("are u ok?"))
+            newsList.add(news)
+        }
+        return newsList
+    }
+
+    private fun getRadomLengthString(str: String): String {
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n) {
+            builder.append(str)
+        }
+        return builder.toString()
+    }
+
+    inner class NewsAdapter(val newsList: List<News>) :
+        RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val newsTitle: TextView = view.findViewById(R.id.newsTitle)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+            val holder = ViewHolder(view)
+            holder.itemView.setOnClickListener {
+                val news = newsList[holder.adapterPosition]
+                if (isTwoPane) {
+                    val fragment = newsContentFrag as NewsContentFragment
+                    fragment.refresh(news)
+                } else {
+                    NewsContentActivity.actionStart(parent.context, news)
+                }
+            }
+            return holder
+        }
+
+        override fun getItemCount() = newsList.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val news = newsList[position]
+            holder.newsTitle.text = news.title
+        }
+
+    }
+
+
+}
+
