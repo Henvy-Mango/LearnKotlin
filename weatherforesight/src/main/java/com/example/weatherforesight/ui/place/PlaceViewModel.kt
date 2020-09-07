023c0d9b0b7a22f863972169c79aa3a7a529f457
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2020-09-08 00:46:01 +0800

    weatherforesight

--- weatherforesight/src/main/java/com/example/weatherforesight/ui/place/PlaceViewModel.kt
+++ weatherforesight/src/main/java/com/example/weatherforesight/ui/place/PlaceViewModel.kt
@@ -23,4 +23,10 @@ class PlaceViewModel : ViewModel() {
     fun searchPlaces(query: String) {
         searchLiveData.value = query
     }
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+
+    fun getPlace() = Repository.getPlace()
+
+    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
