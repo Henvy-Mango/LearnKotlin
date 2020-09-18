2fb476181c58c36bad846afa917b912e5cc09503
Naomi <mangogo.naomi@qq.com>
2020-09-18 23:47:29 +0800

    all be done

--- activity/src/main/java/com/example/kotlin/LifeCircleActivityOne.kt
+++ activity/src/main/java/com/example/kotlin/LifeCircleActivityOne.kt
@@ -1,11 +1,44 @@
 package com.example.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class LifeCircleActivityOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_circle_one)
+    }
+
+    private val TAG = "LifeTime1"
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "OnDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "OnRestart")
     }
 }
